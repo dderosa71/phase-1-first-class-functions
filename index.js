09a8c83ab61ea9845aa15702530828f076e56357
Dominic DeRosa <dderosa71@gmail.com>
2021-07-15 06:24:11 -0500

    commit tree 58cff5a56fae4f19ed4a86fe582b1e1d625dbef8

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(x){
+    x()
+}
+
+function returnsANamedFunction(){
+   return function dom(){}
+    }
+
+function returnsAnAnonymousFunction(){
+    return function(){}
+}
